6afb41e16fa3bf5699c6955c639f0cfd53fd333c
Nathã Hernandez Corrêa <nathahcorrea@gmail.com>
2024-05-25 12:57:41 -0300

    chore: remove unused

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,14 +2,8 @@ export const colors = {
   reset: "\x1b[0m",
   bright: "\x1b[1m",
   black: "\x1b[30m",
-  red: "\x1b[31m", // Código para a cor vermelha
-  green: "\x1b[32m",
   yellow: "\x1b[33m",
-  blue: "\x1b[34m", // Código para a cor azul
-  magenta: "\x1b[35m", // Código para a cor magenta
   cyan: "\x1b[36m",
-  bgBlack: "\x1b[40m",
-  bgGreen: "\x1b[42m",
   bgCyan: "\x1b[46m",
   bgWhite: "\x1b[47m",
 };
